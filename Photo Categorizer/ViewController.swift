f90b852dc3df4bc62ce8a193e3369460d94151bc
miajohansson <johansson.mia09@gmail.com>
2018-01-06 12:03:07 -0800

    Initial Commit

---
+++ Photo Categorizer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Photo Categorizer
+//
+//  Created by Mia Johansson on 1/6/18.
+//  Copyright © 2018 Johansson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
